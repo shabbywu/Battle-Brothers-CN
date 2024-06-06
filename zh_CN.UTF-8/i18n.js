f1dab0dcbf9a55c8481efb28638815bdfb75d99d
shabbywu <shabbywu@qq.com>
2024-06-06 12:04:03 +0800

    Add files via upload

---
+++ zh_CN.UTF-8/i18n.js
@@ -0,0 +1,255 @@
+const translations = {
+    "zh-cn": {
+        // character_screen_left_panel_header_module.js
+        "Name": "名称",
+        "Title": "头衔",
+        "Level Up": "Level Up",
+        "Click here to level up!": "点击此处升级！",
+        "/^Really free (.*)\\?$/": "真的释放$1?",
+        "/^Really dismiss (.*)\\?$/": "真的解雇$1?",
+        "/^(.*) will permanently leave you and place his <br/>current equipment in the stash.$/": "$1 会永远离开你，并将他 <br/>当前的装备放到仓库中。",
+        "Pay": "支付",
+        "/^(\\d+?) Reparations$": "$1 赔偿",
+        "/^(\\d+?) Compensation$": "$1 补偿",
+        // character_screen_perks_module.js
+        "Stash": "仓库",
+        "Perks": "特技",
+        "Unlock Perk": "解锁特技",
+        "Ground": "地面",
+        // loading_screen.js
+        "Tip": "提示",
+        // dialog_screen.js
+        "Are you sure?": "你确定吗？",
+        // load_campaign_menu_module.js
+        "Load Campaign": "载入战役",
+        "Load": "载入",
+        "Delete": "删除",
+        "Cancel": "取消",
+        "Permanently delete the campaign ": "永久删除战役",
+        "- Incompatible Version or DLC Missing -": "- 版本不兼容或缺少DLC -",
+        // new_campaign_menu_module.js
+        "New Campaign": "新战役",
+        "Company Name": "战团名称",
+        "Late Game Crisis": "游戏后期危机",
+        "Random": "随机",
+        "Nobles at War": "贵族战争",
+        "Greenskin Invasion": "绿皮入侵",
+        "Undead Scourge": "亡灵天灾",
+        "Holy War": "圣战",
+        "Permanent Destruction": "永久摧毁",
+        "Economic Difficulty": "经济难度",
+        "Beginner": "初学者",
+        "Veteran": "老兵",
+        "Expert": "专家",
+        "Starting Funds": "启动资金",
+        "High": "高",
+        "Medium": "中",
+        "Low": "低",
+        "Combat Difficulty": "战斗难度",
+        "Ironman Mode": "铁人模式",
+        "Ironman": "铁人模式",
+        "Map Seed": "地图种子",
+        "Unexplored Map": "未探索地图",
+        "Company Origin": "战队起源",
+        "Battle Brothers": "战场兄弟",
+        "Cancel": "取消",
+        "Next": "下一步",
+        "Previous": "上一步",
+        "Start": "开始",
+        // save_campaign_menu_module.js
+        "New Savegame": "新存档",
+        "Save Campaign": "保存战役",
+        "Save": "保存",
+        "Name:": "名称:",
+        "- Incompatible Version -": "- 版本不兼容 -",
+        // main_menu_module.js
+        "Scenarios": "场景",
+        "Options": "选项",
+        "Tutorial Videos": "教程视频",
+        "Credits": "制作人员名单",
+        "Quit": "退出",
+        "Resume": "恢复",
+        "Retire": "退休",
+        "Save & Quit": "保存并退出",
+        "Retreat": "撤退",
+        "Are you sure you want to retire from the company? This will permanently end your campaign and leave the men in charge.": "你确定要从战队中退休吗？这将永远结束你的战役，让别人来负责。",
+        // options_menu_module_audio_panel.js
+        "Video": "视频",
+        "Audio": "音频",
+        "Controls": "控制",
+        "Gameplay": "游戏性",
+        "Volume": "音量",
+        "Master": "整体",
+        "Music": "音乐",
+        "Effects": "音效",
+        "Ambience": "环境",
+        "Advanced": "高级",
+        "Hardware Sound": "硬件声音",
+        // options_menu_module_controls_panel.js
+        "Scrollmode": "滚动模式",
+        "Drag with Mouse": "鼠标拖动",
+        "Edge of Screen": "边缘滚动",
+        "Mouse": "鼠标",
+        "Hardware Cursor": "硬件光标",
+        // options_menu_module_gameplay_panel.js
+        "Camera": "相机",
+        "Always Focus AI Movement": "始终聚焦AI移动",
+        "Auto-Adjust Height Level": "自动调整高度级别",
+        "Always Hide Trees": "总是隐藏树木",
+        "Overlays": "轮廓",
+        "Always Show Hitpoint Bars": "始终显示生命条",
+        "Show Orientation Icons": "显示方向图标", 
+        "Flow": "流畅度",
+        "Faster Player Movement": "更快的玩家移动",
+        "Faster AI Movement": "更快的AI移动",
+        "Auto-End Turns": "自动结束回合",
+        "Auto-Loot": "自动掠夺",
+        "Reset Equipment After Battle": "战斗后重置装备",
+        "Auto-Pause After Leaving City": "离开城市后自动暂停",
+        // options_menu_module_video_panel.js
+        "Resolution": "分辨率",
+        "Display": "显示",
+        "Fullscreen": "全屏",
+        "Window": "窗口化",
+        "Borderless Window": "无边框窗口",
+        "VSync": "垂直同步",
+        "Detail": "细节",
+        "Depth of Field": "景深",
+        "UI Scale": "界面大小",
+        "Scene Scale": "场景大小",
+        "Changes take effect after restart!": "重新启动后更改生效！",
+        "Apply":"应用",
+        "Ok":"确认",
+        // scenario_menu_module.js
+        "Choose a Scenario": "选择一个场景",
+        "Play": "开始",
+        // tactical_combat_result_screen_loot_panel.js
+        "Found Loot": "找到的战利品",
+        // tactical_combat_result_screen.js
+        "Statistics": "统计数据",
+        "Loot": "战利品",
+        "Leave": "离开",
+        "Continue": "继续",
+        // main_menu_screen.js
+        // world_combat_dialog.js
+        "Prepare for Combat": "准备战斗",
+        "You are being attacked": "你被攻击了",
+        "Your scout reports that he has seen the following...": "你的侦察员报告说他看到了以下情况…",
+        // world_obituary_screen.js
+        "Name": "名称",
+        "Days": "天数",
+        "Battles": "战斗",
+        "Kills": "击杀",
+        "Demise": "死因",
+        "No one has fallen since you took command": "没有人死在你的指挥下",
+        "A single man has fallen since you took command": "1个人死在你的指挥下",
+        "/^(\\d+) men have fallen since you took command$/": "$1 个人死在你的指挥下",
+        // world_screen_topbar_daytime_module.js
+        "/^Day (\\d+)$/": "第 $1 天",
+        // world_town_screen_barber_dialog_module.js
+        "Hair Color": "发色",
+        "Head": "头型",
+        "Hair": "发型",
+        "Beard": "胡须",
+        "Body": "体型",
+        "Tattoo": "纹身",
+        // world_campfire_screen_hire_dialog_module.js
+        "Requirements": "要求",
+        "Costs": "花费",
+        "Up Front": "预付款",
+        "Hire": "雇佣",
+        "Hire Follower": "雇佣追随者",
+        // world_town_screen_shop_dialog_module.js
+        "Shop": "商店",
+        // world_town_screen_tavern_dialog_module.js
+        "Share news and rumors with the patrons": "与顾客分享新闻和谣言",
+        "Pay": "支付",
+        "Pay a round for your men": "请你的队员喝一轮",
+        // world_town_screen_taxidermist_dialog_module.js
+        "Nothing can be crafted from your combination of trophies.": "你提供组合无法制作任何东西。",
+        "Craft": "手工制作",
+        // world_town_screen_temple_dialog_module.js
+        "You have no wounded in your ranks that can be treated here.": "队伍里没有受伤的人，不需要在这里接受治疗。",
+        "Have wounds treated so that they heal faster and don\'t worsen while on the road.": "对伤口进行治疗，使伤口愈合更快，在路上不会恶化",
+        // world_town_screen_training_dialog_module.js
+        "You have no one in your ranks that can train here currently.": "队伍中没有人可以在这里训练。",
+        "Lease the training grounds and pay for lessons to mold your men into experienced fighters.": "租用训练场地并支付课程费，把你的人塑造成有经验的战士。",
+        "No training options available at this time.": "目前没有可用的训练选项。",
+        "Engage!": "开战！",
+        "To Arms!": "拿起武器！",
+        "Obituary": "讣告",
+    }
+}
+
+
+function $_(text) {
+    const keys = Object.keys(translations["zh-cn"]);
+    for (var idx = 0; idx < keys.length; idx++) {
+        const key = keys[idx];
+        if (key[0] == "/" && key[key.length - 1] == "/") {
+            const matcher = new RegExp(key.substring(1, key.length - 2));
+            if (matcher.test(text)) {
+                return text.replace(matcher, translations["zh-cn"][key]);
+            }
+        } else {
+            if (key === text) {
+                return translations["zh-cn"][key];
+            }
+        }
+    }
+    return text;
+}
+
+function $_node(node) {
+    // patch element which is not wrapped with jquery
+    if (node === undefined || node === null) {
+        return node;
+    }
+    if (node.childNodes !== undefined) {
+        for (var idx in node.childNodes) {
+            var child = node.childNodes[idx];
+            if (child.nodeType == document.TEXT_NODE) {
+                child.nodeValue = $_(child.nodeValue);
+            } else if (child.childNodes !== undefined) {
+                $_node(child);
+            }
+        }
+    }
+    // patch element which is wrapped with jquery
+    else if (node instanceof $) {
+        node.children().map(function (idx, child) {
+            if (child.nodeType == document.TEXT_NODE) {
+                child.nodeValue = $_(child.nodeValue);
+            } else if (child.childNodes !== undefined) {
+                $_node(child);
+            }
+        })
+    }
+    return node;
+}
+
+
+function hookNodeOperator(origin) {
+    return function () {
+        var nodes = [];
+        for (var idx in arguments) {
+            var node = arguments[idx];
+            nodes.push($_node(node));
+        }
+        return origin.apply(this, nodes);
+    }
+}
+
+Element.prototype.append = hookNodeOperator(Element.prototype.append);
+Element.prototype.appendChild = hookNodeOperator(Element.prototype.appendChild);
+$.fn.append = hookNodeOperator($.fn.append)
+$.fn.appendChild = hookNodeOperator($.fn.appendChild)
+$.fn.changeButtonText = function(_text)
+{
+    var result = this.find('.label:first');
+    if (result.length > 0)
+    {
+        result.html($_(_text));
+    }
+    return null;
+};
